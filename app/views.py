ac4a3b88b4433ae0ca3238ae4e99474188be52d2
Georges Duverger <gduverger@ebay.com>
2014-10-22 19:27:46 -0400

    Delete tests

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 
 from app import forms as app_forms
 from app import models as app_models
+#from app import webkit2png
 
 
 def index(request):
@@ -16,7 +17,7 @@ def index(request):
     return render_to_response('index.html', args, context_instance=RequestContext(request))
 
 
-def upload(request):
+def create(request):
     if request.method == 'POST':
         test_form = app_forms.Test(request.POST, request.FILES)
         if test_form.is_valid():
@@ -31,3 +32,10 @@ def upload(request):
     }
 
     return render_to_response('index.html', args, context_instance=RequestContext(request))
+
+
+def delete(request, test_id):
+    test = get_object_or_404(app_models.Test, id=test_id)
+    test.delete()
+    # TODO delete media file
+    return redirect('index')
