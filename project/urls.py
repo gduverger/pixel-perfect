03cb81810798f4abd82c1e17d37a04c7ed5faf7b
Georges Duverger <gduverger@ebay.com>
2014-10-22 15:54:42 -0400

    Add files and directories

---
+++ project/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import app.views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', app.views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
+
+)
