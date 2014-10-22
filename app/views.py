6b4e144891379cb7c66f0dd2f5a645407d23f897
Georges Duverger <gduverger@ebay.com>
2014-10-22 17:23:15 -0400

    Static diff

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.template import RequestContext
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!!')
+    
+    return render_to_response('index.html', {}, context_instance=RequestContext(request))
