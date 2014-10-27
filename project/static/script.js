b7bbb19264a1aedc92712d3a346f3d7343390e0e
Georges Duverger <gduverger@ebay.com>
2014-10-27 10:59:47 -0400

    Add fake prod

--- project/static/script.js
+++ project/static/script.js
@@ -29,7 +29,6 @@ $(function() {
             prod = $screenshot.attr( "src" ),
             onComplete = function( data ) { complete( data, $screenshot ); };
 
-        console.log( mock, prod );
         resemble( mock ).compareTo( prod ).onComplete( onComplete );
     }
 
@@ -45,9 +44,48 @@ $(function() {
     }
 
     resemble.outputSettings({
-        largeImageThreshold: 999999
+        largeImageThreshold: 0
     });
 
     diffAll();
+
+    function getBase64Image(img) {
+
+        // Create an empty canvas element
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Copy the image contents to the canvas
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        // Get the data-URL formatted image
+        // Firefox supports PNG and JPEG. You could check img.src to
+        // guess the original format, but be aware the using "image/jpg"
+        // will re-encode the image.
+        var dataURL = canvas.toDataURL("image/png");
+
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+
+    function getBase64FromImageUrl(URL) {
+        var img = new Image();
+        img.src = URL;
+        img.onload = function() {
+
+            var canvas = document.createElement("canvas");
+            canvas.width = this.width;
+            canvas.height = this.height;
+
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(this, 0, 0);
+
+            var dataURL = canvas.toDataURL("image/png");
+
+            return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+
+        };
+    }
 
 });
