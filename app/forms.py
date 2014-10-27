fe43219b9c2a081d5dd37eb4197f97a5403d371a
Georges Duverger <gduverger@ebay.com>
2014-10-27 14:52:49 -0400

    Improve UI

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm
+from django import forms as django_forms
 
 from app import models as app_models
 
-class Test(ModelForm):
+class Test(django_forms.ModelForm):
     class Meta:
         model = app_models.Test
         fields = ['name', 'file', 'link']
+        widgets = {
+            'link': django_forms.TextInput(attrs={'value': 'http://www.ebay.com/today'}),
+        }
