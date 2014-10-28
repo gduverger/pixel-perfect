1532eddafaa48a52f9e4329b7d4704e3ccdca29f
Georges Duverger <gduverger@ebay.com>
2014-10-28 11:04:53 -0400

    Add conf files

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
 import base64
 import requests
 
+import project import settings
+
 from app import forms as app_forms
 from app import models as app_models
 
@@ -8,13 +10,6 @@ try:
     import simplejson as json
 except ImportError:
     import json
-
-
-BROWSERSTACK_USERNAME = 'gduverger'
-BROWSERSTACK_PASSWORD = 'mHKhd7NvNgdJ8EoKxARp'
-#BROWSERSTACK_AUTHORIZATION = 'Z2R1dmVyZ2VyOm1IS2hkN052TmdkSjhFb0t4QVJw'
-BROWSERSTACK_URL = 'http://www.browserstack.com/screenshots'
-BROWSERSTACK_CALLBACK_URL = 'http://0.0.0.0:5000/callback'
 
 BROWSERS = [
     {
@@ -36,7 +31,7 @@ BROWSERS = [
 def get_headers():
     return {
         'Content-Type': 'application/json;charset=utf-8',
-        'Authorization': base64.b64encode(str.encode('%s:%s' % (BROWSERSTACK_USERNAME, BROWSERSTACK_PASSWORD))),
+        'Authorization': base64.b64encode(str.encode('%s:%s' % (settings.BROWSERSTACK_USERNAME, settings.BROWSERSTACK_PASSWORD))),
         #'Accept': 'application/json',
         #'Content-Length': 1
     }
@@ -52,7 +47,7 @@ def get_json_response(request):
 
 
 def get_screenshots(test):
-    url = '%s/%s.json' % (BROWSERSTACK_URL, test.browserstack_job_id)
+    url = '%s/%s.json' % (settings.BROWSERSTACK_URL, test.browserstack_job_id)
     request = requests.get(url)
     json_response = get_json_response(request)
     if json_response and json_response.get('state') == 'done':
@@ -78,7 +73,7 @@ def request_screenshots(test):
         "browsers": BROWSERS
     }
 
-    request = requests.post(BROWSERSTACK_URL, data=json.dumps(data), headers=get_headers())
+    request = requests.post(settings.BROWSERSTACK_URL, data=json.dumps(data), headers=get_headers())
     json_response = get_json_response(request)
 
     if json_response:
