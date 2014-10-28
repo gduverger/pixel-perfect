d473d1fb540c994f7854f6c12fcb4c8c9a6f9b05
Georges Duverger <gduverger@ebay.com>
2014-10-28 14:00:52 -0400

    Remove conf files

--- project/settings.py
+++ project/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 import dj_database_url
 
-from project.confs import conf
-from project.confs import conf_dev
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
